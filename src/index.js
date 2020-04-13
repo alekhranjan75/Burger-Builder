fb63183856139d80266144911439042b3875e105
alekhranjan75 <alekhranjan75@gmail.com>
2020-04-13 20:07:03 +0530

    Burger App setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/app';
+
+ReactDOM.render( <App appName = "Person Manager"/>, document.getElementById('root'));
